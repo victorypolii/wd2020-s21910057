e83bd994d6507800cf69dcbf981c84bcd39765fc
victorypolii <s21910057@student.unklab.ac.id>
2020-11-11 15:53:13 +0800

    Javascript Exercise

--- Pertemuan_20/script.js
+++ Pertemuan_20/script.js
@@ -42,10 +42,44 @@
 // console.log(kondisi2);
 
 //PROGRAM BMI JAVASCRIPT
-let massa=prompt("Masukkan Massa badan (kg) : ");
-let tinggi=prompt("Masukkan Tinggi badan (cm) : ");
-tinggi/=100;
-let bmi=massa/(tinggi*tinggi);
-console.log("BMI anda adalah "+bmi.toFixed(2));
-let kategori=bmi>25? "GEMUK": bmi<=18?"KURUS":"NORMAL";
-console.log("Kategori Anda " + kategori);
+// let massa=prompt("Masukkan Massa badan (kg) : ");
+// let tinggi=prompt("Masukkan Tinggi badan (cm) : ");
+// tinggi/=100;
+// let bmi=massa/(tinggi*tinggi);
+// console.log("BMI anda adalah "+bmi.toFixed(2));
+// let kategori=bmi>25? "GEMUK": bmi<=18?"KURUS":"NORMAL";
+// console.log("Kategori Anda " + kategori);
+
+//PROGRAM BMI JAVACRIPT WITH FUNCTION
+let massa,tinggi,bmi,kategori,roc;//roc=risk of comorbidities
+function bmiCalculate() {
+    massa=prompt("Masukkan Massa badan (kg) : ");
+    tinggi=prompt("Masukkan Tinggi badan (cm) : ");
+    tinggi/=100;
+    bmi=massa/(tinggi*tinggi);
+    if (bmi<18.5) {
+        kategori="Underweight";
+        roc="Low";
+    }else if (bmi>=18.5&&bmi<25) {
+        kategori="Normal renge";
+        roc="Average";
+    } else if (bmi>=25&&bmi<30) {
+        kategori="Overweight";
+        roc="Mildly increased";
+    } else if (bmi>=30&&bmi<35) {
+        kategori="Obese Class I";
+        roc="Moderate";
+    } else if (bmi>=35&&bmi<40) {
+        kategori="Obese Class II";
+        roc="Severe";
+    } else if (bmi>=40) {
+        kategori="Obese Class III";
+        roc="Very Severe";
+    }  
+    console.log("Weight : "+massa+" kg");
+    console.log("Height : "+tinggi+" m2");
+    console.log("BMI : "+bmi.toFixed(1)+" kg/m2");
+    console.log("Classification : "+kategori);
+    console.log("Risk of Comorbidities : "+roc);
+}
+bmiCalculate()
