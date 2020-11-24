25cd8207770f0c27dac2a8581f9769c51624488d
victorypolii <s21910057@student.unklab.ac.id>
2020-11-24 16:19:49 +0800

    Javascript Object

---
+++ Pertemuan_25/script.js
@@ -0,0 +1,30 @@
+// mendeklarasikan object literal
+// let buah={
+//     nama: 'apel',
+//     warna: 'hijau',
+//     harga: '5000'
+// }
+// const saya={
+//     nama: 'saya',
+//     umur: 13,
+//     job: 'student'
+// }
+// console.log(buah);
+// console.log(buah.nama);
+// console.log(buah["warna"]);
+// saya.umur=20;
+// saya["Alamat"]="Bintuni";
+// console.log(saya);
+// delete saya.job;
+// console.log(saya)
+
+const saya={
+    nama: 'saya',
+    umur: 13,
+    job: 'student',
+    sayHai: function () {
+        console.log("Hai");
+    }
+}
+console.log(saya);
+saya.sayHai();
